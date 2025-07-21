2d0b937826b18375d470038c46dbc5266b64433b
afalshadidi <afalshadidi@gmail.com>
2025-07-21 20:50:37 +0300

    Initial commit

--- myshop/urls.py
+++ myshop/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('store/', include('store.urls')),        # لاحقًا للمنتجات
+    path('accounts/', include('accounts.urls')),  # لاحقًا للتسجيل والدخول
 ]
+
+
